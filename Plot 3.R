501fc8c766af5a9bf8eb19addf2986f7f5a36825
xiaoyanzheng2 <41071640+xiaoyanzheng2@users.noreply.github.com>
2018-08-23 15:52:22 +0800

    Update Plot 3.R

--- Plot 3.R
+++ Plot 3.R
@@ -10,7 +10,9 @@ unzip('./data/household_power_consumption.zip', exdir = './data')
 ## HPC= Household Power Consumption
 Txtfile <- file('./data/household_power_consumption.txt')
 ## Assign downloaded data to HPC"household power consumption"
-#HPC <- read.table(text = grep("^[1,2]/2/2007",readLines(Txtfile),value=TRUE), sep = ';', col.names = c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), na.strings = '?')
+HPC1 <- read.table(Txtfile, stringsAsFactors = FALSE, sep = ";", header = TRUE, na.strings = "?", dec = ".")
+HPC1$Date <- as.Date(HPC1$Date, format = "%d/%m/%Y")
+HPC <- subset(HPC1, Date == "2007-02-01" | Date == "2007-02-02")
 
 # Plot 3
 png(file = "./data/plot3.png", width = 480, height = 480, units="px")
